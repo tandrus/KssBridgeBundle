c2a9d124ad93a35fe5c2a7b66a1303aaff6ced79
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-01-31 14:00:33 -0700

    Updates Example with dynamic menus and pages
    KSS Bundle example dynamically creates a menu by getting
    all the top level sections. Styleguide.html.twig page removed
    and replaced with a more dynamic reference.html.twig page that
    can be used to render all sections in the styleguide through
    the single action.

--- Controller/ExampleController.php
+++ Controller/ExampleController.php
@@ -19,14 +19,66 @@ class ExampleController extends Controller
     }
 
     /**
-     * @Route("/styleguide")
+     * @Route("/{reference}/{title}", requirements={"reference" = "(\d+\.?)+"})
      * @Template
      */
-    public function styleguideAction()
+    public function referenceAction($reference, $title = null)
     {
-        $kss = new Parser($this->getRequest()->server->get('DOCUMENT_ROOT') . '/bundles/scankss/css');
+        $parser = $this->getKssParser();
+        $section = $parser->getSection($reference);
+        if (!$section->getReference()) {
+            throw $this->createNotFoundException('Reference ' . $reference . ' does not exist in the Styleguide!');
+        }
+        $sections = $parser->getSectionChildren($section->getReference());
+
         return array(
-            'kss' => $kss,
+            'section' => $section,
+            'sections' => $sections
         );
+    }
+
+    /**
+     * @Route("/fragment/menu")
+     * @Template
+     */
+    public function menuAction()
+    {
+        $links = array();
+        $links[] = array(
+            'name' => 'Home',
+            'title' => '',
+            'url' => $this->generateUrl('scan_kss_example_index'),
+        );
+
+        $parser = $this->getKssParser();
+        $sections = $parser->getTopLevelSections();
+        foreach ($sections as $section) {
+            $link = array(
+                'name' => $section->getReference() . '. ' . $section->getTitle(),
+                'title' => $section->getDescription(),
+                'url' => $this->generateUrl(
+                    'scan_kss_example_reference',
+                    array(
+                        'reference' => $section->getReference(),
+                        'title' => strtolower(preg_replace('/\W+/', '-', $section->getTitle())),
+                    )
+                ),
+            );
+            $links[] = $link;
+        }
+
+        return array(
+            'links' => $links,
+        );
+    }
+
+    /**
+     * Returns a KSS Parser loaded with the CSS files from the bundle
+     *
+     * @return Parser
+     */
+    protected function getKssParser()
+    {
+        return new Parser('../Resources/public/css');
     }
 }
